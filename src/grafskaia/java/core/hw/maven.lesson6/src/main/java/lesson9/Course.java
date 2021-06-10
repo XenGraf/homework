6ab7eb73a6e04e83b0ee03ce4c85b435cc978b84
XenGraf <79160125+XenGraf@users.noreply.github.com>
2021-06-10 19:41:33 +0300

    Homework9

---
+++ src/grafskaia/java/core/hw/maven.lesson6/src/main/java/lesson9/Course.java
@@ -0,0 +1,24 @@
+package lesson9;
+
+public class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                '}';
+    }
+}
